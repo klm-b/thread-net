978c85a5f34cbe73b262c324481bacb61b1bfa6e
klm-b <klm_b@outlook.com>
2022-01-01 17:14:18 +0300

    Update settings

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:44344',
-    gyazoAccessToken: ''
+    apiUrl: 'https://localhost:5001',
+    gyazoAccessToken: 'TOKEN'
 };
 
 /*
